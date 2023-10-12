2f8e0d152366995e59de9f60e92c0ded83edfd26
Athana Silva <athanasilva95@gmail.com>
2023-10-12 17:05:42 -0300

    class

--- CS50_MTI/agree.c
+++ CS50_MTI/agree.c
@@ -5,13 +5,15 @@ int main(void)
 {
     char c = get_char("Do you agree? \n");
 
-    if (c == 'y')
+    if (c == 'y' || c == 'Y')
     {
         printf("Agreed.\n");
     }
 
-    else if (c == 'n')
+    else if (c == 'n' || c == 'N')
     {
         printf("Not agreed.\n");
     }
 }
+
+// || -> is the same to 'or'
---
+++ CS50_MTI/meow.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// int main(void)
+// {
+//     int i = 0;
+//     while (i < 3)
+//     {
+//         printf("Meow\n");
+//         i++;
+//     }
+// }
+
+// alternative sintaxe using for loop
+
+int main(void)
+{
+    for (int i = 0;i < 3;i++)
+    {
+        printf("Meow\n");
+    }   
+}
