b78a896d8504467834c8b6c58b92d7c184127d4c
Athana Silva <athanasilva95@gmail.com>
2023-10-15 17:40:51 -0300

    debug code

---
+++ CS50_MTI/buggy0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    for (int i = 0;i <= 3;i++)
+    {   
+        //printf("i is %i\n", i);
+        printf("#\n");
+    }
+}
+
+// use printf like a debug code, you can see exacly what is inside your code.
