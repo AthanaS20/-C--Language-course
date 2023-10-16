82dbaeb5615c0ff5f1147e81c066c40e4d5e5e4f
Athana Silva <athanasilva95@gmail.com>
2023-10-16 17:44:17 -0300

    arrays

--- CS50_MTI/scores.c
+++ CS50_MTI/scores.c
@@ -1,12 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float avarage(int array[]);
+const int N = 3;
+
 int main(void)
 {   
-    int scores[3]; // this is how we store data in a list
-    scores[0] = 75;
-    scores[1] = 80;
-    scores[2] = 33;
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) 3);
+    int scores[3]; // this is how we store data in a list
+    for (int i = 0;i < 3;i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+    printf("Average: %f\n", avarage(scores));
+    
+}
+
+float avarage(int array[])
+{   
+    int sum = 0;
+    for (int i = 0; i < N;i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) N;
 }
