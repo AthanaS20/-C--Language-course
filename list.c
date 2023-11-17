c70c7ae4eb43bf4c8cb800f948b79d75797f24c1
Athana Silva <athanasilva95@gmail.com>
2023-11-17 19:14:23 -0300

    update

--- list.c
+++ list.c
@@ -10,12 +10,29 @@ int main(void)
         return 1;
     }
 
+    int *temp = malloc(4 * sizeof(int));
+
+    if (temp == NULL)
+    {   
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        temp[i] = list[i]; 
+    }
+
+    temp[3] = 4;
+    free(list);
+    list = temp;
+
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
     for (int i = 0; i < 3; i++)
     {
-        printf("%i\n", i);
+        printf("%i\n", list[i]);
     }
 }
