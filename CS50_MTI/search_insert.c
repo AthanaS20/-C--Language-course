d1d7db327ec1b1924a70e1dbd72a14c1f8db1ec0
Athana Silva <athanasilva95@gmail.com>
2023-11-24 20:25:22 -0300

    update code

--- CS50_MTI/search_insert.c
+++ CS50_MTI/search_insert.c
@@ -4,22 +4,21 @@ int main(void) {
     int s[] = {5, 2, 3, 6, 8, 4};
     int target = 9;
     size_t numsSize = sizeof(s) / sizeof(s[0]); // Corrige o cálculo do tamanho do array
-    int first = 0;
-    int last = numsSize - 1;
+    int target_index = 0;
 
-    while (first <= last) {
-        int mid = first + (last - first) / 2; // Corrige a fórmula para calcular mid
-
-        if (s[mid] == target) {
-            printf("O alvo %d foi encontrado no índice %d\n", target, mid);
-            return 0; // Termina o programa com código de sucesso
-        } else if (s[mid] < target) {
-            first = mid + 1;
-        } else {
-            last = mid - 1;
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (s[i] == target || s[i] > target)
+        {
+            printf("%i\n",target_index = i);
+            break;
+        }
+        else if (s[i] < target && i == numsSize - 1)
+        {
+            printf("%i\n",target_index = i + 1);
+            break;
         }
     }
+    return target_index;
 
-    printf("O alvo %d não foi encontrado. Deve ser inserido no índice %d\n", target, first);
-    return 0; // Termina o programa com código de sucesso
 }
